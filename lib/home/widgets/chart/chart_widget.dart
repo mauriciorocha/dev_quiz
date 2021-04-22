7f1c425b504a14780215d83d78268bf1bd7b3b1b
Mauricio Santos da Rocha <mauricio.santosrocha@gmail.com>
2021-04-22 09:19:59 -0300

    First commit
    First commit

---
+++ lib/home/widgets/chart/chart_widget.dart
@@ -0,0 +1,38 @@
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class ChartWidget extends StatelessWidget {
+  final int score;
+  const ChartWidget({Key? key, required this.score}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      width: 80,
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              height: 80,
+              width: 80,
+              child: CircularProgressIndicator(
+                strokeWidth: 10,
+                value: score.toDouble() / 100,
+                backgroundColor: AppColors.chartSecondary,
+                valueColor:
+                    AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "$score%",
+              style: AppTextStyles.heading,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
